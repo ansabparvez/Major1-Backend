46f519b9089b5fd133860bc234b2640a3adf8a05
Mohd Ansab Parvez <ansab0parvez@gmail.com>
2021-11-22 00:19:50 +0530

    Added api to check availability of username. Added api to register a new user.

--- functions/controllers/user.js
+++ functions/controllers/user.js
@@ -3,11 +3,12 @@ const express = require('express')
 const cors = require('cors')
 const admin = require('firebase-admin')
 const authMiddleware = require('./authMiddleware');
+const uuid = require('uuid')
 
 const firestore = admin.firestore();
 const userApp = express();
 userApp.use(cors({origin: true}));
-//userApp.use(authMiddleware);
+userApp.use(authMiddleware);
 
 exports.users = functions.https.onRequest(userApp);
 
@@ -24,15 +25,38 @@ userApp.get("/isRegistrationFinished", async(request, response) => {
     response.status(201).send(JSON.stringify(jsonResponse));
 })
 
+userApp.get("/isUserNameAvailable", async(request, response) => {
+    const userName = request.query.userName;
+    const usersCollection = firestore.collection("Users/");
+    console.log("user name is "+userName)
+    const snapShot = await usersCollection.where("userName", '==', userName).get()
+    var isUserNameAvailable = false;
+    if(snapShot.empty){
+        console.log("snapshot is empty")
+        isUserNameAvailable = true
+    }
+
+    const jsonResponse = {
+        "success":true,
+        "isUserNameAvailable":isUserNameAvailable
+    };
+
+    response.status(201).send(JSON.stringify(jsonResponse));
+})
+
 userApp.post("/completeRegistration", async(request, response) => {
-    //const userId = request.user.uid;
-    const userId = "dgst43bvfsdt43"
+    const userId = request.user.uid;
+    //const userId = "dgst43bvfsdt43"
     const userDocument = firestore.doc("Users/"+userId);
-    const userData = {...request.body};
+    var userData = {...request.body};
+    userData.userId = userId;
+    userData.anonymousId = uuid.v4().replace(/\-/g,'');
+
+    await userDocument.set(userData);
+
     const jsonResponse = {
         "success":true
     };
-    await userDocument.set(userData);
 
     response.status(201).send(JSON.stringify({...jsonResponse}));
 })
